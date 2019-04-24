dd5aaa2a971b3f3570b3b44102a36d51860e2b6e
gitikasoni <gitikasoni.18@gmail.com>
2019-04-24 22:31:02 +0530

    incorporated review comment

--- java-examples/src/com/examples/java/Addition.java
+++ java-examples/src/com/examples/java/Addition.java
@@ -2,22 +2,20 @@ package com.examples.java;
 
 import java.util.Scanner;
 
-
-
 public class Addition {
-	
-	public static void main(String args[]){
-		
+
+	public static void main(String args[]) {
+
 		float a, b, c;
 		System.out.println("Enter first number");
-		Scanner scan  = new Scanner (System.in);
-		a= scan.nextFloat();
+		Scanner scan = new Scanner(System.in);
+		a = scan.nextFloat();
 		System.out.println("Enter second number");
-		b= scan.nextFloat();
-		
-		c=a+b;
-	   System.out.print("The sum of two numbers is " +c);
-	   
-	}		
+		b = scan.nextFloat();
+
+		c = a + b;
+		System.out.print("The sum of two numbers is " + c);
+
+	}
 
 }
